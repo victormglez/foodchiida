e18de451de22076eac9336c404c9de292f27ce0a
victormglez <victormgb99@gmail.com>
2024-04-21 20:35:20 -0600

    Fontawesome added

--- index.php
+++ index.php
@@ -200,6 +200,8 @@
   <!-- Footer -->
     <!-- jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/b292d5edc4.js" crossorigin="anonymous"></script>
     <!-- Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- Sweetalert -->
